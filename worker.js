1a132b0c21b08fe8e93bb739d92bf3a592bda64c
Emmanuel Aboderin <aboderinemmanuel@gmail.com>
2024-12-10 10:38:56 +0100

    save to domain in actuality

--- worker.js
+++ worker.js
@@ -23,9 +23,6 @@ const generateLastModifiedDateFilter = (date, nowDate, propertyName = 'hs_lastmo
 };
 
 const saveDomain = async domain => {
-  // disable this for testing purposes
-  return;
-
   domain.markModified('integrations.hubspot.accounts');
   await domain.save();
 };
